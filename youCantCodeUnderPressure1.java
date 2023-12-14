c8fa5efd580a9797a9ac46a58690f7fd8b18f06a
Mason <chtuff@gmail.com>
2023-12-14 12:03:27 -0500

    forgot to save

---
+++ youCantCodeUnderPressure1.java
@@ -0,0 +1,18 @@
+/*PARAMETERS: Go as fast as you can
+ * RESULT: double the integer and return it
+ * EXAMPLE: 
+ * PSEUDOCODE: double a number.
+ */
+class Java {
+  public static int doubleInteger(int i) {
+    // Double the integer and return it!
+    return i+i;
+  }
+}
+/*Other solution I thought was clever */
+class Java {
+  public static int doubleInteger(int i) {
+    // Double the integer and return it!
+    return i<<1;
+  }
+}
