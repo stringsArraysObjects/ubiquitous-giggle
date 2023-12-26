577061613352ee9e0886c4aff7e64c8f3ae3806e
Chris <chtuff@gmail.com>
2023-12-26 16:52:29 -0500

    need to study this solution

--- highestToLowest.java
+++ highestToLowest.java
@@ -1,15 +1,19 @@
+/*PARAMETERS:All numbers are valid Int32, no need to validate them.
+         There will always be at least one number in the input string.
+RESULT:   Output string must be two numbers separated by a single space, 
+          and highest number is first.
+EXAMPLE: highAndLow("1 9 3 4 -5"); // return "9 -5"
+PSEUDOCODE:  Convert to List 
+             
+*/
 
-
-
-
-
-
-
-import static java.util.Arrays.stream;
-
-class Kata {
-  static String highAndLow(String numbers) {
-    var stats = stream(numbers.split(" ")).mapToInt(Integer::parseInt).summaryStatistics();
-    return stats.getMax() + " " + stats.getMin();
-  }
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+/*not my solution */
+public class Kata {
+    public static String highAndLow(String numbers) {
+        List<Integer> nums = Arrays.stream(numbers.split(" ")).map(Integer::parseInt).toList();
+        return "%s %s".formatted(Collections.max(nums), Collections.min(nums));
+    }
 }
