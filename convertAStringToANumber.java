d2727ef0fccb79bcec51b036a7503f99d87bd612
Chris <chtuff@gmail.com>
2024-01-26 12:16:26 -0500

    solution

---
+++ convertAStringToANumber.java
@@ -0,0 +1,6 @@
+//PARAMETER: all inputs will be strings,
+//           and every string is a perfectly 
+//           valid representation of an integral number
+//RESULT:   We need a function that can transform a string into a number
+//EXAMPLE:  "1405" --> 1405
+//PSEUDOCODE: change the data type from a string input to an integer output
