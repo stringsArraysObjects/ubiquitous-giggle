91bca0578eb4f0a52b28c9090ae59972d3dcc1b6
Chris <chtuff@gmail.com>
2024-01-26 12:16:55 -0500

    solution

--- convertAStringToANumber.java
+++ convertAStringToANumber.java
@@ -4,3 +4,10 @@
 //RESULT:   We need a function that can transform a string into a number
 //EXAMPLE:  "1405" --> 1405
 //PSEUDOCODE: change the data type from a string input to an integer output
+
+import java.lang.*;
+public class StringToNumber {
+  public static int stringToNumber(String str) {
+    return Integer.valueOf(str).intValue();
+  }
+}
