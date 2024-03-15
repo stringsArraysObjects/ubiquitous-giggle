501ae8096086ae180878a6419f6c9db2ef5bf386
Chris <chtuff@gmail.com>
2024-03-14 21:31:03 -0400

    solution

---
+++ duplicateEncoder.java
@@ -0,0 +1,9 @@
+/*PARAMETER: ignore capitalization when 
+*            determining if a character is a duplicate.
+*RESULT:     convert a string to a new string where each
+*            character in the new string is "(" if that character 
+*            appears only once in the original string, or ")" if 
+*            that character appears more than once in the original string.
+*EXAMPLE:    "din"      =>  "((("
+*            "recede"   =>  "()()()"
+*
