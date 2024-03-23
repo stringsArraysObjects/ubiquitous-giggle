1e88f3dcad18290bbbad83118caf009fb23a7bbd
Chris <chtuff@gmail.com>
2024-03-23 11:22:20 -0400

    solution

---
+++ sumOfDigits.java
@@ -0,0 +1,7 @@
+/*PARAMETERS: The input will be a non-negative integer.
+*RESULT:      Given n, take the sum of the digits of n. 
+*             If that value has more than one digit, continue
+*             reducing in this way until a single-digit number is produced.
+*EXAMPLE:      16  -->  1 + 6 = 7
+*              942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+*/
