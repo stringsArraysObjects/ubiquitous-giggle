973d4b06abe9ec8b3df62404987c58142bb8617f
Chris <chtuff@gmail.com>
2024-03-23 11:24:05 -0400

    solution

--- sumOfDigits.java
+++ sumOfDigits.java
@@ -5,3 +5,10 @@
 *EXAMPLE:      16  -->  1 + 6 = 7
 *              942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
 */
+
+public class DRoot {
+  public static int digital_root(int n) {
+    return (1 + (n - 1) % 9);
+  }
+}
+
