7eb93a31a5a9813b517d65e4ba96d6953d6aaed7
Chris <chtuff@gmail.com>
2024-06-12 18:01:12 -0400

    solution

---
+++ feast.java
@@ -0,0 +1,16 @@
+/*Write a function feast that takes the animal's name and dish as arguments and returns true or false 
+* to indicate whether the beast is allowed to bring the dish to the feast.
+* There is just one rule: the dish must start and end with the same letters as the animal's name
+* assertTrue(Kata.feast("chickadee","chocolate cake"));
+  assertTrue(Kata.feast("marmot","mulberry tart"));*/
+
+
+public class Kata {
+
+  public static boolean feast(String beast, String dish) {
+    
+    return beast.charAt(0)==dish.charAt(0) && beast.charAt(beast.length()-1) == dish.charAt(dish.length()-1);
+    
+  }
+  
+}
