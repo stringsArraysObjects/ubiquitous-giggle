5bd65b26904a8f4fef8c91a966694830ad3b0edc
Chris <chtuff@gmail.com>
2024-07-08 13:28:17 -0400

    solution

---
+++ getAge.java
@@ -0,0 +1,12 @@
+/*
+*PARAMETER: Assume the test input string is always a valid string
+*           The first character in the string is always a number
+*RESULT:    Write a program that returns the girl's age (0-9) as an integer
+*PSEUDOCODE:Invoke the parseInt method that can be used to convert strings 
+*           to primitive numbers.
+*/ 
+
+
+function getAge(inputString){
+  return parseInt(inputString);
+}
