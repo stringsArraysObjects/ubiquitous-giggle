6728a7993252ac278cb35d3534297a96ee23e3a1
Mason <chtuff@gmail.com>
2024-07-29 10:36:36 -0400

    solution

--- counter.java
+++ counter.java
@@ -1,16 +1,19 @@
-// We need a function that counts the number of sheep present in the array (true means present).
+/*We need a function that counts the number of sheep present in the array (true means present).
 
-// For example,
+For example,
 
-// [true,  true,  true,  false,
-//   true,  true,  true,  true ,
-//   true,  false, true,  false,
-//   true,  false, false, true ,
-//   true,  true,  true,  true ,
-//   false, false, true,  true]
-// The correct answer would be 17.
+[true,  true,  true,  false,
+true,  true,  true,  true ,
+true,  false, true,  false,
+true,  false, false, true ,
+true,  true,  true,  true ,
+false, false, true,  true]
+The correct answer would be 17.
+Hint: Don't forget to check for bad values like null/undefined
 
-// Hint: Don't forget to check for bad values like null/undefined
+PSEUDOCODE:use the frequency method od the collection class to return 
+an integer representing the number of times the specified object appears in the collection.
+*/
 
 
 
